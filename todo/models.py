a77c8ee68a328cb21c969840f24ca79a2c99b3d1
Zainab Alugo <zainabalugo@gmail.com>
2025-02-23 16:04:53 +0100

    todo models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
-# Create your models here.
+import uuid
+class Todo(models.Model):
+    id= models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    completed= models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.title
