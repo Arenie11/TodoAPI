5d4bab279455514781c4c754c83c4fe6441d6583
Zainab Alugo <zainabalugo@gmail.com>
2025-02-23 16:18:15 +0100

    user model

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 class Todo(models.Model):
     id= models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     title = models.CharField(max_length=50)
@@ -8,6 +9,8 @@ class Todo(models.Model):
     updated = models.DateTimeField(auto_now=True)
     completed= models.BooleanField(default=False)
     
-    
     def __str__(self):
         return self.title
+class User(models.Model):
+    username= models.OneToOneField(User, on_delete=models.CASCADE)
+    password= models.CharField(max_length=10)
