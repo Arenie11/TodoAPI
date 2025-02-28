fe4bc1f1352df986906e49a9d0ab3b5a43fe6309
Zainab Alugo <zainabalugo@gmail.com>
2025-02-28 16:59:09 +0100

    app url

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns= [
-    
+    path('gettodo/', views.TodoView.as_views()),
+    path('<str:id>/', views.SingleTodoView.as_views()),
+    path('<register/', views.RegistrationView.as_views()),
 ]
