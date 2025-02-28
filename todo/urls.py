255a321ae3cc17fab4ebee4c5d3c5bd505586ccd
Zainab Alugo <zainabalugo@gmail.com>
2025-02-28 17:03:21 +0100

    url.py correction

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns= [
-    path('gettodo/', views.TodoView.as_views()),
-    path('<str:id>/', views.SingleTodoView.as_views()),
-    path('<register/', views.RegistrationView.as_views()),
+    path('gettodo/', views.TodoView.as_view()),
+    path('<str:id>/', views.SingleTodoView.as_view()),
+    path('register/', views.RegistrationView.as_view()),
 ]
